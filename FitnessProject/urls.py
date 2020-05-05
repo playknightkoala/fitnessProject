aa11c532c4830d96db7a28a9e6d289d1c4693c6b
yshongBit <playknightkoala@gmail.com>
2020-05-05 23:23:30 +0800

    [upload] 1. 修正圖片無法顯示問題 2. 試做navbar 3. 新增網頁icon

--- FitnessProject/urls.py
+++ FitnessProject/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include ('Home.urls')),
+    url(r'^', include('Home.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
